cab19a58695c091345112ee6febe928c5d76510a
Stacy Tran <sttran@uwcu.org>
2024-01-08 14:06:51 -0600

    <completed exercise>

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,24 @@
 // Declare and assign the variables below
-
+let shuttlename = "Determination";
+let shuttlespeed = 17500;
+let distancetoMars = 225000000;
+let distancetoMoon = 384400;
+const milesperkm = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
-// Calculate a space mission below
-
+console.log(typeof shuttlename);
+console.log(typeof shuttlespeed);
+console.log(typeof distancetoMars);
+console.log(typeof distancetoMoon);
+console.log(typeof shuttlename);
+// Calculate a space mission below - days to reach mars
+let milestoMars = distancetoMars * milesperkm;
+let hourstoMars = milestoMars / shuttlespeed;
+let daystoMars = hourstoMars / 24;
 // Print the results of the space mission calculations below
-
+console.log(shuttlename + " will take " + daystoMars + " days to reach Mars.");
 // Calculate a trip to the moon below
-
+let milestoMoon = distancetoMoon * milesperkm;
+let hourstoMoon = milestoMoon / shuttlespeed;
+let daystoMoon = hourstoMoon /24;
 // Print the results of the trip to the moon below
+console.log(shuttlename + " will take " + daystoMoon + " days to reach Moon.");
